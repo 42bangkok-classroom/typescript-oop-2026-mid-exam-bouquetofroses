63e5f7d69a7776fde9292bd3f14f24cf6290b32e
bouquetofroses <valenny1402@gmail.com>
2026-01-21 14:09:22 +0700

    4

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,18 @@
 // Write your code below
+const n6 = Number(process.argv[2]);
+
+if (isNaN(n6) || n6 <= 0) {
+  process.exit(0);
+}
+
+for (let i = 1; i <= n6; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
