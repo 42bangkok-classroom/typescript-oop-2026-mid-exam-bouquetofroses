72a894b8edf756f500c6476a5b7a84d96cdbaf61
bouquetofroses <valenny1402@gmail.com>
2026-01-21 16:21:30 +0700

    15

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -3,12 +3,15 @@ const n4 = Number(process.argv[2]);
 for (let i = 0 ; i < n4 ; i++ ) {
     let rs4 = "";
 
-    for (let k = 0 ; k > n4-1-i  ; k++){
+    for (let k = 0 ; k < n4-1-i  ; k++){
         rs4 += " " ;
         for (let j = 0 ; j < n4-k  ; j++){
             rs4 += j;
         }
     }
+    // for (let j = 0 ; j < n4-k  ; j++){
+    //     rs4 += j;
+    // }
 
     console.log(rs4);
 }
